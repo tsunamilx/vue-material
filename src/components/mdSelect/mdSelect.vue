a39867524a26be1db6375cc65b36f9af8b6db6ce
lixiao <lixiao@LiXiaodeMacBook-Pro.local>
2017-06-07 18:44:43 +0800

    build

--- src/components/mdSelect/mdSelect.vue
+++ src/components/mdSelect/mdSelect.vue
@@ -93,10 +93,23 @@
       setParentPlaceholder() {
         this.parentContainer.hasPlaceholder = !!this.placeholder;
       },
+
+      reset() {
+        console.log('reset select');
+        const optionsArray = Object.keys(this.options).map((el) => this.options[el]);
+  
+        optionsArray.forEach((el) => {
+          el.check = false;
+        });
+      },
+
       selectOptions(modelValue) {
         const optionsArray = Object.keys(this.options).map((el) => this.options[el]);
 
         if (optionsArray && optionsArray.length && modelValue && modelValue.indexOf) {
+          optionsArray.forEach((el) => {
+            //el.check = false;
+          });
           optionsArray.filter((el) => modelValue.indexOf(el.value) !== -1)
             .forEach((el) => {
               el.check = true;
